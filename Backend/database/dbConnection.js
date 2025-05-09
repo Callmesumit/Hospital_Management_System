ed10a280b97ce390fb69dc15d2d8081ce353f5f5
SUMIT_DEVDA <113769249+Callmesumit@users.noreply.github.com>
2025-05-09 10:48:21 +0530

    Add files via upload

--- Backend/database/dbConnection.js
+++ Backend/database/dbConnection.js
@@ -1,1 +1,16 @@
+import mongoose  from "mongoose";
 
+
+export const dbConnection = () => {
+    mongoose
+    .connect(process.env.MONGO_URI, {
+        dbName: "HOSPITAL_MANAGEMENT_SYSTEM",
+    })
+    .then(()=>{
+        console.log("Yeh! Finally Database Connected");
+    })
+    .catch((err) => {
+        console.log(`Some error occured while connectiing to database: ${err}`);
+    });
+
+}
